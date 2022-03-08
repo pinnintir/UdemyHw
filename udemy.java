dc9eb38f91ad8ce0f4617246a1afc7c2f6e00900
pinnintir <99298515+pinnintir@users.noreply.github.com>
2022-03-08 18:15:23 -0500

    Add files via upload

---
+++ udemy.java
@@ -0,0 +1,20 @@
+public class Application { 
+
+    public static void main(String[] args) { 
+        int myNumber = 88; 
+
+        short myShort = 847; 
+        long myLong = 9797;
+        
+        double myDouble = 7.23243;
+        float myfloat = 324.3f;
+        char myChar = "y"; 
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myfloat);
+        System.out.println(myChar);
+    }
+}
