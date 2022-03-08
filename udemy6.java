4b824105e6e12c6555e668572601a36eac993c7f
pinnintir <99298515+pinnintir@users.noreply.github.com>
2022-03-08 18:16:31 -0500

    Add files via upload

---
+++ udemy6.java
@@ -0,0 +1,16 @@
+public static void main(String[] args) { 
+
+    Scanner scanner = new Scanner(System.in); 
+
+    int value = 0; 
+    do { 
+            System.out.println("Enter a number: "); 
+            value = scanner.nextInt(); 
+
+    } 
+    while(value !=5);
+    System.out.println("Got 5!");
+
+
+    }
+}
